c4cbbc528b42e0f5aeffd3bd82f8685083e098a3
telwoo <106715923+telwoo@users.noreply.github.com>
2022-09-08 09:46:13 -0400

    Add files via upload

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -2,25 +2,40 @@
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+let map = L.map('mapid').setView([44.0, -80.00], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-79.63059997559999,43.6772003174]}}
+]};
 
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/telwoo/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-  console.log(data);
-  // Creatng a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-    color: "#ffffa1",
-    weight: 2,
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-      + feature.properties.dst + "</h3>"); 
-    }
-  })
-  .addTo(map);
-});
+      console.log(layer);
+      layer.bindPopup();
+     }
+}).addTo(map);
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -29,8 +44,8 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/til
     accessToken: API_KEY
 });
 
-
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
 
 
